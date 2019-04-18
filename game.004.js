9eea7ac0b3753014ec77c94743720d8d4f1bfe8f
julianeskin <julian.eskin@gmail.com>
2019-04-18 14:12:14 -0400

    Add files via upload

--- game.004.js
+++ game.004.js
@@ -5,14 +5,18 @@ Game.Objects = [];
 Game.FPS = 30;
 Game.Currencies =  [['Second','Seconds','secs'], // this is some kind of time unit, I need to decide what – not implemented yet
 					['Kelvin','Kelvin','kelv'], // not sure how/if this will be used
-					['Quantum Foam','Quantum Foam','qfoams'],
-					['Baryon','Baryons','baryons'],
-					['Atom','Atoms','atoms']];
-Game.Rates = [0,0,1,0,0]; // starting point for testing - reduce for release
-Game.Wallet = [0,0,0,0,0]; // starting point for testing - reduce all to 0 for release
+					['Quantum Foam','Quantum Foam','qfoam'],
+					['Elementary Particle','Elementary Particles','elementary'],
+					['Subatomic Particle','Subatomic Particles','subatomic'],
+					['Atom','Atoms','atom']];
+var starting_point = [];
+for (var i in Game.Currencies) {starting_point[i] = 0;}
+
+Game.Rates = starting_point.slice(); // starting point for testing - reduce for release
+Game.Wallet = [0,0,88,0,0,0]; // starting point for testing - reduce all to 0 for release
 
 Game.Nobjects = 1;
-Game.Object = function(id,type,singular,plural,basecosts,costgrowth,production,number,minreq,maxreq,color)
+Game.Object = function(id,type,singular,plural,basecosts,costgrowth,production,number,minreq,maxreq)
 {
     this.singular = singular;
 	this.plural = plural;
@@ -32,7 +36,7 @@ Game.Object = function(id,type,singular,plural,basecosts,costgrowth,production,n
 	return this;
 }
 
-Game.ActiveItem = 'quantum';
+Game.ActiveItem = '';
 
 function lookup(object) {return document.getElementById(object);}
 
@@ -47,15 +51,34 @@ function AddEvent(object,event,fcn)
 }
 
 Game.Load = function() {
-	new Game.Object('quantumgenerator1','quantum','Quantum Field Equation','Quantum Field Equations',[0,0,10,0,0],[0,0,1,0,0],[0,0,0.2,0,0],0,[0,0,0,0,0],[100,10,100000000,100000000,100000000]);
-	new Game.Object('quantumgenerator2','quantum','Quantum Field Actuator','Quantum Field Actuators',[0,0,100,0,0],[0,0,1,0,0],[0,0,1,0,0],0,[0,0,50,0,0],[100,10,100000000,100000000,100000000]);
-	new Game.Object('quantumgenerator3','quantum','Quantum Field Fluctuator','Quantum Field Fluctuators',[0,0,1000,0,0],[0,0,1,0,0],[0,0,12,0,0],0,[0,0,500,0,0],[100,10,100000000,100000000,100000000]);
-	new Game.Object('baryongenerator1','baryon','Baryon Generator1','Baryon Generator1',[0,0,5000,0,0],[0,0,1,0,0],[0,0,0,1,0],0,[0,0,2000,0,0],[100,100000000,100000000,100000000,100000000]);
-	new Game.Object('baryongenerator2','baryon','Baryon Generator2','Baryon Generator2',[0,0,20000,0,0],[0,0,1,0,0],[0,0,0,7,0],0,[0,0,15000,0,0],[100,100000000,100000000,100000000,100000000]);
-	new Game.Object('baryongenerator3','baryon','Baryon Generator3','Baryon Generator3',[0,0,80000,0,0],[0,0,1,0,0],[0,0,0,21,0],0,[0,0,50000,0,0],[100,100000000,100000000,100000000,100000000]);
-	new Game.Object('atomgenerator1','atom','Atom Generator1','Atom Generator1',[0,0,0,555,0],[0,0,0,1,0],[0,0,0,0,1],0,[0,0,0,255,0],[100,100000000,100000000,100000000,100000000]);
-	new Game.Object('atomgenerator2','atom','Atom Generator2','Atom Generator2',[0,0,0,5555,0],[0,0,0,1,0],[0,0,0,0,16],0,[0,0,0,2555,0],[100,100000000,100000000,100000000,100000000]);
-	new Game.Object('atomgenerator3','atom','Atom Generator3','Atom Generator3',[0,0,0,55555,0],[0,0,0,1,0],[0,0,0,0,42],0,[0,0,0,25555,0],[100,100000000,100000000,100000000,100000000]);
+//	unique ID, type, Singular Name, Plural Name, 
+//	base costs, cost growth exponents, production rates, number, minimum requirements, maximum requirements
+ 	new Game.Object('quantumgenerator1','qfoam','Quantum Field Equation','Quantum Field Equations',
+		[0,0,10,0,0,0],[0,0,1.02,0,0,0],[0,0,0.1,0,0,0],0,[0,0,0,0,0,0],[100,10,100000000,100000000,100000000,100000000]);
+	new Game.Object('quantumgenerator2','qfoam','Quantum Field Actuator','Quantum Field Actuators',
+		[0,0,100,0,0,0],[0,0,1.02,0,0,0],[0,0,1.2,0,0,0],0,[0,0,50,0,0,0],[100,10,100000000,100000000,100000000,100000000]);
+	new Game.Object('quantumgenerator3','qfoam','Quantum Field Fluctuator','Quantum Field Fluctuators',
+		[0,0,1000,0,0,0],[0,0,1.02,0,0,0],[0,0,15,0,0,0],0,[0,0,500,0,0,0],[100,10,100000000,100000000,100000000,100000000]);
+	new Game.Object('elementary1','elementary','Sphaleron','Sphalerons',
+		[0,0,5000,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],0,[0,0,2000,0,0,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('elementary2','elementary','Symmetry Violator','Symmetry Violators',
+		[0,0,20000,0,0,0],[0,0,1,0,0,0],[0,0,0,7,0,0],0,[0,0,15000,0,0,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('elementary3','elementary','Quark Mixing Matrix','Quark Mixing Matrices',
+		[0,0,80000,0,0,0],[0,0,1,0,0,0],[0,0,0,35,0,0],0,[0,0,50000,0,0,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('subatomic1','subatomic','Spin Operator','Spin Operators',
+		[0,0,0,555,0,0],[0,0,0,1,0,0],[0,0,0,0,0.33,0],0,[0,0,0,255,0,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('subatomic2','subatomic','Strong Interaction','Strong Interactions',
+		[0,0,0,5555,0,0],[0,0,0,1,0,0],[0,0,0,0,4.44,0],0,[0,0,0,2555,0,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('subatomic3','subatomic','Relativistic Renormalizer','Relativistic Renormalizers',
+		[0,0,0,55555,0,0],[0,0,0,1,0,0],[0,0,0,0,55.5,0],0,[0,0,0,25555,0,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('atom1','atom','Hydrogen Condenser','Hydrogen Condenser',
+		[0,0,0,0,90,0],[0,0,0,0,0.99,0],[0,0,0,0,0,1],0,[0,0,0,0,45,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('atom2','atom','Isotope Organizer','Isotope Organizers',
+		[0,0,0,0,1800,0],[0,0,0,0,0.99,0],[0,0,0,0,0,17.5],0,[0,0,0,0,900,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('atom3','atom','Quantum Degenerator','Quantum Degenerators',
+		[0,0,0,0,36000,0],[0,0,0,0,0.99,0],[0,0,0,0,0,350],0,[0,0,0,0,18000,0],[100,100000000,100000000,100000000,100000000,100000000]);
+	new Game.Object('atom4','atom','Heavy Element Engine (Star)','Heavy Element Engines (Stars)',
+		[0,0,0,0,0,720000],[0,0,0,0,0,0.99],[0,0,0,0,0,7000],0,[0,0,0,0,360000,0],[100,100000000,100000000,100000000,100000000,100000000]);
 
 	Game.ItemMenu();
 	Game.GeneratorMenu();
@@ -107,7 +130,7 @@ Game.Loop = function()
 
 Game.RateUpdate = function()
 {
-	Game.Rates = [0,0,0,0,0];
+	Game.Rates = starting_point.slice();
 	for (var i in Game.Objects)
 		{
 			var object = Game.Objects[i];
@@ -121,7 +144,7 @@ Game.RateUpdate = function()
 Game.Logic = function()
 {
 	if (Game.Nobjects > 0 ){
-		var amounts = [0,0,0,0,0];
+		var amounts = starting_point.slice();
 		for (var i = 0; i < amounts.length; i++ ) {
 			amounts[i] = Game.Rates[i]/Game.FPS;
 		}
@@ -131,9 +154,8 @@ Game.Logic = function()
 
 Game.getCost = function(item,number)
 {
-	var prices = [0,0,0,0,0];
+	var prices = starting_point.slice();
  	var item = Game.Objects[item];
-
 	for (var i = 0; i < Game.Rates.length; i++) {
 		prices[i] = Math.ceil(item.basecosts[i] * Math.pow(item.costgrowth[i],item.number));
 	}
@@ -200,10 +222,10 @@ Game.Draw = function() //rename this
 					lookup(generator.id + '_production').innerHTML =  Math.round(generator.production[j]*generator.number*10)/10 + ' ' + Game.Currencies[j][1] + ' per sec (' + Math.round(generator.production[j]*10)/10 + ' each).';
 				}
 			}
-			// check if minimum requirements are met (see below)
-//			lookup(generator.id + '_button') MAKE VISIBLE
+		//// check if minimum requirements are met
+			lookup(generator.id + '_button').style = 'display:block;';
 		} else {
-//			lookup(generator.id + '_button') MAKE HIDDEN
+			lookup(generator.id + '_button').style = 'display:none;';
 		}
 	}
 
@@ -268,16 +290,25 @@ Game.ItemMenu = function()
 		itemtable += '</div>';
 	}
 	lookup('items').innerHTML = itemtable;
+	
+	var items = Game.Currencies.slice(2,Game.Currencies.length);
+	for (var j in items) {
+		var item = items[j][2];
+		try{throw item}
+		catch(thing){
+			AddEvent(lookup(thing + '_button'),'click',function(what){return function(e){Game.ActiveItem = thing;};}(thing));
+		}
+	}
 }
 
 Game.GeneratorMenu = function() {
 	var generatortable = [];
  	for (var i in Game.Objects) {
  		var generator = Game.Objects[i];
- 		generatortable += '<div id="' + generator.id + '_button" class="generatorbutton active visible">';
+ 		generatortable += '<div id="' + generator.id + '_button" class="generatorbutton active" style="display:none;">';
   		generatortable += '<div id="' + generator.id + '_number" class="generatornumber"></div>'; 		
   		generatortable += '<div id="' + generator.id + '_title" class="generatortitle">'+generator.plural+'</div>';
-		generatortable += '<div class="eachcosts">Each costs</div>';
+		generatortable += '<div class="eachcosts">Making 1 costs</div>';
 		generatortable += '<div id="' + generator.id + '_cost" class="generatorcost"></div>';
 		generatortable += '<div class="generating">Generating</div>';
 		generatortable += '<div id="' + generator.id + '_production" class="generatorproduction"></div>';
@@ -287,8 +318,7 @@ Game.GeneratorMenu = function() {
  	
 	for (var k in Game.Objects) {
 		var generator = Game.Objects[k];
-		generator.lookup = lookup(generator.id + '_button');
-		AddEvent(generator.lookup,'click',function(what){return function(e){Game.Purchase(what,1);};}(generator.singular));
+		AddEvent(lookup(generator.id + '_button'),'click',function(what){return function(e){Game.Purchase(what,1);};}(generator.singular));
 	}
 }
 
@@ -301,10 +331,9 @@ window.onload = function()
 document.addEventListener('keydown',function(event) {
     var control = 0;
     if (event.keyCode == 65 || event.keyCode == 87 || event.keyCode == 69 || event.keyCode == 70 || event.keyCode == 32) { // A-W-E-F-space
-		//Game.Produce([0,0,10,10,10]);
-		var amounts = [0,0,0,0,0];
+		var amounts = starting_point.slice();
 		for (var i = 0; i < amounts.length; i++ ) {
-			amounts[i] = 30 * Game.Rates[i]/Game.FPS;
+			amounts[i] = 300 * Game.Rates[i]/Game.FPS; //30 seconds worth of FPS
 		}
 		Game.Produce(amounts);
     }

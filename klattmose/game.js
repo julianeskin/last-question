ed2fd6c65c8092262e05090ef2ee12f393cc0fa5
klattmose <mattklose@gmail.com>
2019-04-23 20:57:15 -0400

    Made my own folder
    If we're coding at the same time, we don't want to get in each other's way. Should probably use forks and stuff, but whatever.
    
    Also, I implemented a save feature

---
+++ klattmose/game.js
@@ -0,0 +1,442 @@
+var version = 0.09;
+var Univ = {};
+Univ.FPS = 30;
+Univ.Items = [];
+Univ.Objects = [];
+Univ.T = 0;
+Univ.SaveTo = 'LastQuestion';
+
+Univ.Item = function(singular,plural,type,number,production,consumption){
+ 	this.singular = singular;
+	this.plural = plural;
+	this.type = type;
+ 	this.number = number;
+	this.production = production; 	// this might be a function in the future,
+	// to allow for nonlinear effects, or to trigger events when certain items get produced
+	
+	this.consumption = consumption; // this might be a function in the future,
+	// to allow for nonlinear effects, or to trigger events when certain items get consumed
+	
+	Univ.Items[this.type] = this;
+	return this;
+}
+
+Univ.Object = function(id,type,singular,plural,number,infoblurb,VisibilityFcn,CostFcn,ProductionFcn,ConsumptionFcn){
+	this.id = id;
+	this.type = type;
+    this.singular = singular;
+	this.plural = plural;
+	this.number = number;
+ 	this.activenumber = number;
+	
+	this.isAffordable = function(howmany){
+		var notenough = 0;
+		for (var item in this.Costs(howmany)) {
+			if (this.Costs(howmany)[item] > Univ.Items[item].number) {
+				notenough++;
+			}
+		}
+		if (notenough == 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	this.Costs = CostFcn;
+	this.Production = ProductionFcn;
+	this.Consumption = ConsumptionFcn;
+	this.isVisible = VisibilityFcn;
+	this.isClickable = function(){ // eventually this will check both if it's affordable, and if other conditions are met
+		if (this.isAffordable) {
+			// Check other conditions here
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	this.Buy = function(howmany){
+		if (this.isAffordable(howmany)) {
+			for (var item in this.Costs(howmany)) {
+				Univ.Transact('spend', item, this.Costs(howmany)[item]);
+			}
+			this.number += howmany;
+			Univ.Logic();
+			this.showPopup();
+		}
+	}
+	this.infoblurb = infoblurb;
+	this.targetactivity = 100; // percent of this generator chosen to be active
+	
+	this.makePopup = function() {
+	// this makes a generic popup for all Generators. In the future maybe some of them will need distinct info or options...
+		var popup = '<div id="left_arrow_black" class="left_arrow_black"></div>';
+		popup += '<div id="left_arrow_background" class="left_arrow_background"></div>';
+		popup += '<div class="popup">';
+		popup += '<div class="popupinfoblurb">' + this.infoblurb + '</div>';
+		var totalproduction = 0;
+		var totalconsumption = 0;
+		for (var item in Univ.Items) {
+			if (item != 'secs' && item != 'kelvin') {
+				if ( typeof this.Production(this.activenumber)[item] !== 'undefined' && this.Production(this.activenumber)[item] > 0 ) {
+					totalproduction++;
+				}
+				if ( typeof this.Consumption(this.activenumber)[item] !== 'undefined' && this.Consumption(this.activenumber)[item] > 0 ) {
+					totalconsumption++;
+				}
+			}
+		}
+		var productionTxt = [];
+		var consumptionTxt = [];
+
+		for (var item in Univ.Items) {
+			if (item != 'secs' && item != 'kelvin') {
+				if (totalproduction > 0 && typeof this.Production(this.activenumber)[item] !== 'undefined') {
+					productionTxt += 'Generating ' + Math.round(this.Production(this.activenumber)[item] * 10)/10 + ' ' + Univ.Items[item].plural + ' per sec (' + Math.round(this.Production(this.activenumber)[item]/this.activenumber*10)/10 + '&nbsp;each). ';
+				}
+				if (totalconsumption > 0 && typeof this.Consumption(this.activenumber)[item] !== 'undefined') {
+					consumptionTxt += 'Consuming ' + Math.round(this.Consumption(this.activenumber)[item] * 10)/10 + ' ' + Univ.Items[item].plural + ' per sec (' + Math.round(this.Consumption(this.activenumber)[item]/this.activenumber*10)/10 + '&nbsp;each). ';
+				}
+			}
+		}
+		popup += '<div class="popup_production">' + productionTxt + '</div><div class="popup_consumption">' + consumptionTxt + '</div>';
+		
+		if ( this.number > 0 ) {
+			popup += '<div class="slidercontainer" id="' + this.id + '_slidercontainer">Target activity: ';
+			popup += '<div id="' + this.id + '_sliderlabel" class="sliderlabel"></div>';
+			popup += '<input type="range" min="0" max="100" value="' + this.targetactivity + '" class="slider" id="' + this.id + '_slider"></div>';
+			popup += '<div id="' + this.id + '_currentlyactive" class="currentactive"></div>';
+		}
+		
+		popup += '</div>';
+		return popup;
+	}
+	
+	this.showPopup = function() {
+		lookup('popupcontainer').innerHTML = this.makePopup();
+		try{throw this.id}
+		catch(generator){
+			lookup(this.id + '_slider').oninput = function(){Univ.updateSlider(generator);};
+		}
+		Univ.updateSlider(this.id);
+		
+		var buttonposition = lookup(this.id + '_button').getBoundingClientRect();
+		var buttonTop = buttonposition.top + window.scrollY;
+		var buttonBot = buttonposition.bottom + window.scrollY;
+		var buttonY = (buttonTop + buttonBot) / 2;
+		
+		var buttonLeft = buttonposition.left + window.scrollX;
+		var buttonLeft = buttonposition.left + window.scrollX;
+		
+		var PopupTop = Math.max(0, buttonY - lookup('popupcontainer').getBoundingClientRect().height / 2);
+		var PopupLeft = buttonLeft + buttonposition.width - 6;
+		var ArrowTop = lookup('popupcontainer').getBoundingClientRect().height / 2;
+
+		lookup('popupcontainer').style.top = PopupTop + 'px';
+		lookup('popupcontainer').style.left = PopupLeft + 'px';
+		lookup('left_arrow_black').style.top = ArrowTop - 14 + 'px';
+		lookup('left_arrow_background').style.top = lookup('left_arrow_black').style.top;
+				
+// eventually also modify width and height of the popup if they need to be bigger than the default for some things...
+		lookup('popupcontainer').style.visibility = 'visible';
+	}
+	
+	Univ.Objects[this.id] = this;
+	return this;
+}
+
+Univ.ActiveItem = 'qfoam';
+
+function lookup(object) {return document.getElementById(object);}
+
+function AddEvent(object,event,fcn){
+	if(object.attachEvent) 
+		object.attachEvent('on' + event, function() {
+			fcn.call(object);
+		});
+	else if(object.addEventListener) 
+	object.addEventListener(event,fcn,false);
+}
+
+Univ.WriteSave = function(mode){
+	var save = {
+		version: version,
+		Objects: {},
+		Items: {}
+	};
+	
+	for(var g in Univ.Objects){
+		save.Objects[g] = {};
+		save.Objects[g].number = Univ.Objects[g].number;
+		save.Objects[g].targetactivity = Univ.Objects[g].targetactivity;
+	}
+	
+	for(var i in Univ.Items){
+		save.Items[i] = {};
+		save.Items[i].number = Univ.Items[i].number;
+	}
+	
+	
+	if(mode == 3){
+		return JSON.stringify(save, null, 2);
+	}
+	else{
+		localStorage.setItem(Univ.SaveTo, JSON.stringify(save));
+	}
+}
+
+Univ.LoadSave = function(data){
+	var save = null;
+	var str = '';
+	
+	if(data){
+		str = data;
+	}else{
+		if(localStorage.getItem(Univ.SaveTo)) str = localStorage.getItem(Univ.SaveTo);
+	}
+		
+	if(str != ''){
+		save = JSON.parse(str);
+	}
+	
+	if(!save) return;
+	
+	if(save.version >= 0.08){
+		for(var g in save.Objects){
+			if(Univ.Objects[g]){
+				Univ.Objects[g].number = save.Objects[g].number;
+				Univ.Objects[g].targetactivity = save.Objects[g].targetactivity;
+			}
+		}
+		
+		for(var i in save.Items){
+			if(Univ.Items[i]){
+				Univ.Items[i].number = save.Items[i].number;
+			}
+		}
+	}
+	
+}
+
+
+
+Univ.Loop = function(){
+ 	Univ.Logic();
+	Univ.RefreshDisplay();
+	setTimeout(Univ.Loop,1000/Univ.FPS);
+}
+
+Univ.Logic = function(){
+// Find the number of each Generator that can run (by checking their consumption needs)
+	for (var g in Univ.Objects) {
+ 		var generator = Univ.Objects[g];
+ 		generator.activenumber = 0;
+ 		for (var i = 1; i <= generator.number; i++) {
+ 			var items_checked = 0;
+ 			var items_satisfied = 0;
+ 			for (var item in Univ.Items) {
+ 				items_checked++;
+ 				if ( typeof generator.Consumption(i)[item] == 'undefined' || generator.Consumption(i)[item] <= Univ.Items[item].number ){
+ 					items_satisfied++;
+				}
+ 			}
+ 			if ( items_satisfied == items_checked ) {
+				generator.activenumber = Math.min(i,Math.round(generator.number * generator.targetactivity/100));
+			}
+ 		}
+ 	}
+
+// Update rates and spend/earn the Items accordingly
+	for (var i in Univ.Items) {
+		var item = Univ.Items[i];
+		item.production = 0;
+		item.consumption = 0;
+		for (var h in Univ.Objects) {
+			var generator = Univ.Objects[h];
+			if ( typeof generator.Production(generator.activenumber)[i] !== 'undefined') {
+				item.production += generator.Production(generator.activenumber)[i];
+			}
+			if ( typeof generator.Consumption(generator.activenumber)[i] !== 'undefined') {
+				item.consumption += generator.Consumption(generator.activenumber)[i];
+			}
+		}
+		Univ.Transact('spend',item.type, item.consumption / Univ.FPS);
+		Univ.Transact('gain',item.type, item.production / Univ.FPS);
+	}
+	
+	if (Univ.toSave || (Univ.T % (Univ.fps * 60) == 0 )) {
+		//check if we can save : no minigames are loading // Yes, I copied this from Cookie Clicker. I did warn you
+		var canSave=true;
+		/*for (var i in Game.Objects)
+		{
+			var me=Game.Objects[i];
+			if (me.minigameLoading){canSave=false;break;}
+		}*/
+		if (canSave) Univ.WriteSave();
+	}
+	
+	Univ.T++; // In case we don't want to run certain parts of code every frame
+}
+
+Univ.Transact = function(transaction,item,amount){
+	if (transaction == 'spend') {
+		Univ.Items[item].number -= amount*1;
+	}
+	if (transaction == 'gain') {
+		Univ.Items[item].number += amount*1;
+	}
+}
+
+Univ.RefreshDisplay = function(){
+	Univ.UpdateItems();
+	Univ.UpdateGenerators();
+}
+
+Univ.UpdateItems = function(){
+	for (var i in Univ.Items) {
+		var item = Univ.Items[i];
+		if (item.type != 'secs' && item.type != 'kelvin') {		
+			// UPDATE NUMBER
+			lookup(item.type + '_number').innerHTML = Math.floor(item.number);
+			
+			// UPDATE TITLE
+			if (item.number == 1) {
+				lookup(item.type + '_title').innerHTML = item.singular;
+			} else {
+				lookup(item.type + '_title').innerHTML = item.plural;
+			}
+			
+			// UPDATE INCOME/SPENDING
+			if (item.production > item.consumption) {
+				lookup(item.type + '_production').innerHTML = '+' + Math.round((item.production-item.consumption) * 10)/10 + ' per sec (+' + Math.round(item.production*10)/10 + '/-' + Math.round(item.consumption*10)/10 +')';
+			} else if  (item.production < item.consumption) {
+				lookup(item.type + '_production').innerHTML = '-' + Math.round((item.production-item.consumption) * 10)/-10 + ' per sec (+' + Math.round(item.production*10)/10 + '/-' + Math.round(item.consumption*10)/10 +')';
+			} else {
+				lookup(item.type + '_production').innerHTML = '';
+			}
+		}
+	}
+}
+
+Univ.UpdateGenerators = function(){
+	for (var i in Univ.Objects) {
+ 		var generator = Univ.Objects[i];
+		if (generator.type == Univ.ActiveItem){
+			// UPDATE NUMBER	
+			lookup(generator.id + '_number').innerHTML = generator.number;
+
+			// UPDATE TITLE
+			if (generator.number == 1) {
+				lookup(generator.id + '_title').innerHTML = generator.singular;
+			} else {
+				lookup(generator.id + '_title').innerHTML = generator.plural;
+			}
+		
+			// UPDATE COST of 1 ( in the future maybe have a way to make more at once
+			var costHTML = 'Cost: ';			
+			for (var item in generator.Costs(1)) {
+				costHTML += generator.Costs(1)[item] + ' ';
+				if (generator.Costs(1)[item] == 1) {
+					costHTML += Univ.Items[item].singular;
+				} else {
+					costHTML += Univ.Items[item].plural;
+				}
+			}
+			lookup(generator.id + '_cost').innerHTML = costHTML;
+
+		//// TO DO: check whether visibility requirements are met by calling generator.isVisible()
+			lookup(generator.id + '_button').style = 'display:block;';
+		//// TO DO: check whether it should be greyed out and unclickable by calling generator.isClickable()
+		} else {
+			lookup(generator.id + '_button').style = 'display:none;';
+		}
+	}
+}
+
+Univ.updateSlider = function(generatorid) {
+	var sliderid = generatorid + '_slider';
+	var slidervalue = lookup(sliderid).value;
+	generator = Univ.Objects[generatorid];
+	generator.targetactivity = slidervalue;
+	
+	lookup(sliderid + 'label').style.left = 90 + Math.round(slidervalue * 0.6) + 'px';
+	lookup(sliderid + 'label').innerHTML = slidervalue + '%';
+	lookup(generatorid + '_currentlyactive').innerHTML = 'Currently active: ' + generator.activenumber + ' (' + Math.round(100 * generator.activenumber / generator.number) + '%)';
+}
+
+Univ.ItemMenuHTML = function(){
+	var itemtable = [];
+
+	for (var item in Univ.Items) {
+		if (item != 'secs' && item != 'kelvin') {
+			itemtable += '<div id="' + item + '_button" class="itembutton active visible">';
+			itemtable += '<div id="' + item + '_number" class="itemnumber"></div>';
+			itemtable += '<div id="' + item + '_title" class="itemtitle"></div>';
+			itemtable += '<div id="' + item + '_production" class="itemproduction"></div>';
+			itemtable += '</div>';
+		}
+	}
+
+	lookup('items').innerHTML = itemtable;
+	
+	for (var item in Univ.Items) {
+		if (item != 'secs' && item != 'kelvin') {
+			try{throw item}
+			catch(item){
+				AddEvent(lookup(item + '_button'),'click',function(what){return function(e){Univ.ActiveItem = item;};}(item));
+			}
+		}
+	}
+}
+
+Univ.GeneratorMenuHTML = function() {
+	var generatortable = [];
+ 	for (var i in Univ.Objects) {
+ 		var generator = Univ.Objects[i];
+ 		generatortable += '<div id="' + generator.id + '_button" class="generatorbutton clickablegenerator" style="display:none;">';
+  		generatortable += '<div id="' + generator.id + '_number" class="generatornumber"></div>'; 		
+  		generatortable += '<div id="' + generator.id + '_title" class="generatortitle">' + generator.plural + '</div>';
+		generatortable += '<div id="' + generator.id + '_cost" class="generatorcost"></div>';
+		generatortable += '<div id="' + generator.id + '_production" class="generatorproduction"></div>';
+		generatortable += '<div id="' + generator.id + '_consumption" class="generatorconsumption"></div>';
+ 		generatortable += '</div>';
+ 	}
+ 	lookup('generators').innerHTML = generatortable;
+ 	
+	for (var k in Univ.Objects) {
+		try{throw Univ.Objects[k]}
+		catch(generator){
+			AddEvent(lookup(generator.id + '_button'),'click',function(what){return function(e){generator.Buy(1);};}(generator.singular));
+			AddEvent(lookup(generator.id + '_button'),'mouseover',function(){return function(){generator.showPopup();};}());
+			AddEvent(lookup(generator.id + '_button'),'mouseout',function(){return function(){lookup('popupcontainer').style.visibility='hidden';};}());
+		}
+	}
+	AddEvent(lookup('popupcontainer'),'mouseover',function(){return function(){lookup('popupcontainer').style.visibility='visible';};}());
+	AddEvent(lookup('popupcontainer'),'mouseout',function(){return function(){lookup('popupcontainer').style.visibility='hidden';};}());
+	
+}
+
+window.onload = function(){
+	Univ.LoadItems();
+	Univ.LoadObjects();
+	Univ.LoadSave();
+	Univ.ItemMenuHTML();
+	Univ.GeneratorMenuHTML();
+ 	Univ.Loop();
+}
+
+AddEvent(window,'keydown',function(e){
+	if (e.ctrlKey && e.keyCode==83) {Univ.toSave=true;e.preventDefault();}//ctrl-s saves the game
+});
+
+// DELETE BEFORE RELEASE (this is so I can speed up production using the A/W/E/F keys)
+document.addEventListener('keydown',function(event) {
+    var control = 0;
+    if (event.keyCode == 65 || event.keyCode == 87 || event.keyCode == 69 || event.keyCode == 70) { // A/W/E/F
+		for (var i in Univ.Items) {
+			var item = Univ.Items[i];
+			Univ.Transact('gain',item.type, item.production / Univ.FPS * 30 );
+		}
+    }
+});
---
+++ klattmose/objects.js
@@ -0,0 +1,307 @@
+Univ.LoadItems = function(){
+// singular,plural,type,number,production_rate,consumption_rate
+	new Univ.Item('Second','Seconds','secs',0,0,0);
+	new Univ.Item('Kelvin','Kelvin','kelvin',0,0,0);
+	new Univ.Item('Quantum Foam','Quantum Foam','qfoam',6000,0,0);	// start with 90 Quantum Foam for testing
+	new Univ.Item('Elementary Particle','Elementary Particles','elementary',0,0,0);
+	new Univ.Item('Subatomic Particle','Subatomic Particles','subatomic',0,0,0);
+	new Univ.Item('Atom','Atoms','atom',0,0,0);
+}
+
+Univ.LoadObjects = function(){
+//	unique ID, type, Singular Name, Plural Name, number, InfoBlurb, Visibility Fcn, Cost Fcn, Production Fcn, Consumption Fcn
+ 	new Univ.Object('quantumgenerator1','qfoam','Quantum Field Equation','Quantum Field Equations',0,
+ 		'Quantum Field Equations are the most basic production unit. Use them to generate Quantum Foam out of nothing.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: Math.ceil(10 * Math.pow(1.02,this.activenumber)) // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				qfoam: 0.1 * number
+			}
+			return production;
+		},
+		function(){ // consumption
+			var consumption = {}
+			return consumption;
+		});
+	new Univ.Object('quantumgenerator2','qfoam','Quantum Field Fluctuator','Quantum Field Fluctuators',0,
+	'Quantum Field Fluctuators generate more Quantum Foam, faster.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: Math.ceil(100 * Math.pow(1.02,this.number))  // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				qfoam: 1.2 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {}
+			return consumption;
+		});
+	new Univ.Object('quantumgenerator3','qfoam','Quantum Field Actuator','Quantum Field Actuators',0,
+	'Quantum Field Actuators generate even more Quantum Foam, even faster. It\'s what the people want. Well, people don\'t exist yet, but they would love this stuff.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: Math.ceil(1000 * Math.pow(1.02,this.number)) // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				qfoam: 15 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {}
+			return consumption;
+		});
+	new Univ.Object('elementary1','elementary','Sphaleron','Sphalerons',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: 5000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				elementary: 1 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				qfoam: 4 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('elementary2','elementary','Symmetry Violator','Symmetry Violators',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: 20000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				elementary: 7 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				qfoam: 14 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('elementary3','elementary','Quark Mixing Matrix','Quark Mixing Matrices',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: 80000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				elementary: 35 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				qfoam: 35 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('subatomic1','subatomic','Spin Operator','Spin Operators',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				elementary: 555 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				subatomic: 0.333 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				elementary: 1 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('subatomic2','subatomic','Strong Interaction','Strong Interactions',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				elementary: 5555 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				subatomic: 4.333 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				elementary: 13 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('subatomic3','subatomic','Relativistic Renormalizer','Relativistic Renormalizers',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				elementary: 55555 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				subatomic: 55.333 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				elementary: 166 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('atom1','atom','Hydrogen Condenser','Hydrogen Condensers',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				subatomic: Math.ceil(90 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				atom: 1 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				subatomic: 1 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('atom2','atom','Isotope Organizer','Isotope Organizers',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				subatomic: Math.ceil(1800 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				atom: 17.5 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				subatomic: 25 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('atom3','atom','Quantum Degenerator','Quantum Degenerators',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				subatomic: Math.ceil(36000 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				atom: 350 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				subatomic: 500 * number
+			}
+			return consumption;
+		});
+	new Univ.Object('atom4','atom','Heavy Element Engine (Star)','Heavy Element Engines (Stars)',0,
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				atom: Math.ceil(720000 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				atom: 7000 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+//				subatomic: 8000 * number,
+				atom: 3000 * number // to simulate turning lower elements into higher ones? -- probably unnecessary
+			}
+			return consumption;
+		});
+}

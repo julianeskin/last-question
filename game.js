a09016bcb4b8e63321890f88ccf58f7045173351
julianeskin <julian.eskin@gmail.com>
2019-04-23 20:29:43 -0400

    Many more items/objects, with placeholder icons

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var version = 0.08;
 var Univ = {};
 Univ.FPS = 30;
+Univ.Speedfactor = 10; // Factor to speed up everything -- for testing.
 Univ.Items = [];
 Univ.Objects = [];
 
@@ -76,7 +77,7 @@ Univ.Object = function(id,type,singular,plural,number,infoblurb,VisibilityFcn,Co
 		var totalproduction = 0;
 		var totalconsumption = 0;
 		for (var item in Univ.Items) {
-			if (item != 'secs' && item != 'kelvin') {
+			if (item != 'secs' && item != 'kelvin' && item != 'computation') {
 				if ( typeof this.Production(this.activenumber)[item] !== 'undefined' && this.Production(this.activenumber)[item] > 0 ) {
 					totalproduction++;
 				}
@@ -89,7 +90,7 @@ Univ.Object = function(id,type,singular,plural,number,infoblurb,VisibilityFcn,Co
 		var consumptionTxt = [];
 
 		for (var item in Univ.Items) {
-			if (item != 'secs' && item != 'kelvin') {
+			if (item != 'secs' && item != 'kelvin' && item != 'computation') {
 				if (totalproduction > 0 && typeof this.Production(this.activenumber)[item] !== 'undefined') {
 					productionTxt += 'Generating ' + Math.round(this.Production(this.activenumber)[item] * 10)/10 + ' ' + Univ.Items[item].plural + ' per sec (' + Math.round(this.Production(this.activenumber)[item]/this.activenumber*10)/10 + '&nbsp;each). ';
 				}
@@ -100,12 +101,10 @@ Univ.Object = function(id,type,singular,plural,number,infoblurb,VisibilityFcn,Co
 		}
 		popup += '<div class="popup_production">' + productionTxt + '</div><div class="popup_consumption">' + consumptionTxt + '</div>';
 		
-		if ( this.number > 0 ) {
-			popup += '<div class="slidercontainer" id="' + this.id + '_slidercontainer">Target activity: ';
-			popup += '<div id="' + this.id + '_sliderlabel" class="sliderlabel"></div>';
-			popup += '<input type="range" min="0" max="100" value="' + this.targetactivity + '" class="slider" id="' + this.id + '_slider"></div>';
-			popup += '<div id="' + this.id + '_currentlyactive" class="currentactive"></div>';
-		}
+		popup += '<div class="slidercontainer" id="' + this.id + '_slidercontainer">Target activity: ';
+		popup += '<div id="' + this.id + '_sliderlabel" class="sliderlabel"></div>';
+		popup += '<input type="range" min="0" max="100" value="' + this.targetactivity + '" class="slider" id="' + this.id + '_slider">';
+		popup += '<div id="' + this.id + '_currentlyactive" class="currentactive"></div></div>';
 		
 		popup += '</div>';
 		return popup;
@@ -118,6 +117,9 @@ Univ.Object = function(id,type,singular,plural,number,infoblurb,VisibilityFcn,Co
 			lookup(this.id + '_slider').oninput = function(){Univ.updateSlider(generator);};
 		}
 		Univ.updateSlider(this.id);
+		if ( this.number > 0 ) {
+			lookup(this.id + '_slidercontainer').style.display = 'block';
+		}
 		
 		var buttonposition = lookup(this.id + '_button').getBoundingClientRect();
 		var buttonTop = buttonposition.top + window.scrollY;
@@ -197,8 +199,8 @@ Univ.Logic = function(){
 				item.consumption += generator.Consumption(generator.activenumber)[i];
 			}
 		}
-		Univ.Transact('spend',item.type, item.consumption / Univ.FPS);
-		Univ.Transact('gain',item.type, item.production / Univ.FPS);
+		Univ.Transact('spend',item.type, Univ.Speedfactor * item.consumption / Univ.FPS);
+		Univ.Transact('gain',item.type, Univ.Speedfactor * item.production / Univ.FPS);
 	}
 }
 
@@ -219,7 +221,7 @@ Univ.RefreshDisplay = function(){
 Univ.UpdateItems = function(){
 	for (var i in Univ.Items) {
 		var item = Univ.Items[i];
-		if (item.type != 'secs' && item.type != 'kelvin') {		
+		if (item.type != 'secs' && item.type != 'kelvin' && item.type != 'computation') {		
 			// UPDATE NUMBER
 			lookup(item.type + '_number').innerHTML = Math.floor(item.number);
 			
@@ -292,8 +294,9 @@ Univ.ItemMenuHTML = function(){
 	var itemtable = [];
 
 	for (var item in Univ.Items) {
-		if (item != 'secs' && item != 'kelvin') {
+		if (item != 'secs' && item != 'kelvin' && item != 'computation') {
 			itemtable += '<div id="' + item + '_button" class="itembutton active visible">';
+			itemtable += '<img src="icons/' + item + '.png" class="itemicon">';
 			itemtable += '<div id="' + item + '_number" class="itemnumber"></div>';
 			itemtable += '<div id="' + item + '_title" class="itemtitle"></div>';
 			itemtable += '<div id="' + item + '_production" class="itemproduction"></div>';
@@ -304,10 +307,10 @@ Univ.ItemMenuHTML = function(){
 	lookup('items').innerHTML = itemtable;
 	
 	for (var item in Univ.Items) {
-		if (item != 'secs' && item != 'kelvin') {
+		if (item != 'secs' && item != 'kelvin' && item != 'computation') {
 			try{throw item}
 			catch(item){
-				AddEvent(lookup(item + '_button'),'click',function(what){return function(e){Univ.ActiveItem = item;};}(item));
+				AddEvent(lookup(item + '_button'),'mouseover',function(what){return function(e){Univ.ActiveItem = item;};}(item));
 			}
 		}
 	}
@@ -337,7 +340,6 @@ Univ.GeneratorMenuHTML = function() {
 	}
 	AddEvent(lookup('popupcontainer'),'mouseover',function(){return function(){lookup('popupcontainer').style.visibility='visible';};}());
 	AddEvent(lookup('popupcontainer'),'mouseout',function(){return function(){lookup('popupcontainer').style.visibility='hidden';};}());
-	
 }
 
 window.onload = function(){
--- objects.js
+++ objects.js
@@ -2,10 +2,32 @@ Univ.LoadItems = function(){
 // singular,plural,type,number,production_rate,consumption_rate
 	new Univ.Item('Second','Seconds','secs',0,0,0);
 	new Univ.Item('Kelvin','Kelvin','kelvin',0,0,0);
-	new Univ.Item('Quantum Foam','Quantum Foam','qfoam',6000,0,0);	// start with 90 Quantum Foam for testing
+	new Univ.Item('Computation','Computations','computation',0,0,0);
+	new Univ.Item('Quantum Foam','Quantum Foam','qfoam',90,0,0);	// start with 90 Quantum Foam for testing
 	new Univ.Item('Elementary Particle','Elementary Particles','elementary',0,0,0);
 	new Univ.Item('Subatomic Particle','Subatomic Particles','subatomic',0,0,0);
-	new Univ.Item('Atom','Atoms','atom',0,0,0);
+	new Univ.Item('Light Atom','Light Atoms','lightatom',0,0,0);
+	new Univ.Item('Heavy Atom','Heavy Atoms','heavyatom',0,0,0);
+	new Univ.Item('Gas Cloud','Gas Clouds','gascloud',0,0,0);
+	new Univ.Item('Nebula','Nebulae','nebula',0,0,0);
+	new Univ.Item('Star','Stars','star',0,0,0);
+	new Univ.Item('Galaxy','Galaxies','galaxy',0,0,0);
+	new Univ.Item('Galactic Cluster','Galactic Clusters','galaxycluster',0,0,0);
+	new Univ.Item('Supercluster','Superclusters','supercluster',0,0,0);
+	new Univ.Item('Planet','Planets','planet',0,0,0);
+	new Univ.Item('Habitable Planet','Habitable Planets','habitableplanet',0,0,0);
+	new Univ.Item('Molecule of Life','Molecules of Life','molecule',0,0,0);
+	new Univ.Item('Homeworld','Homeworlds','homeworld',0,0,0);
+	new Univ.Item('Unicellular Organism','Unicellular Organisms','unicellular',0,0,0);
+	new Univ.Item('Multicellular Organism','Multicellular Organisms','multicellular',0,0,0);
+	new Univ.Item('Sentient Organism','Sentient Organisms','sentient',0,0,0);
+	new Univ.Item('Computing Machine','Computing Machines','computer',0,0,0);
+	new Univ.Item('Planetary Computer','Planetary Computers','planetaryAC',0,0,0);
+	new Univ.Item('Matrioshka Brain','Matrioshka Brains','matrioshka',0,0,0);
+	new Univ.Item('Galactic Computer','Galactic Computer','galacticAC',0,0,0);
+	new Univ.Item('Galactic LAN Party','Galactic LAN Parties','superclusterAC',0,0,0);
+	new Univ.Item('Universal Computer','Universal Computer','universalAC',0,0,0);
+	new Univ.Item('Cosmic Computer','Cosmic Computer','cosmicAC',0,0,0);
 }
 
 Univ.LoadObjects = function(){
@@ -17,7 +39,7 @@ Univ.LoadObjects = function(){
 		},
 		function(howmany){ // costs
 			var prices = {
-				qfoam: Math.ceil(10 * Math.pow(1.02,this.activenumber)) // doesn't work for buying more than 1
+				qfoam: Math.ceil(10 * Math.pow(0.999,this.activenumber)) // doesn't work for buying more than 1
 			}
 			return prices;
 		},
@@ -38,7 +60,7 @@ Univ.LoadObjects = function(){
 		},
 		function(howmany){ // costs
 			var prices = {
-				qfoam: Math.ceil(100 * Math.pow(1.02,this.number))  // doesn't work for buying more than 1
+				qfoam: Math.ceil(100 * Math.pow(0.999,this.number))  // doesn't work for buying more than 1
 			}
 			return prices;
 		},
@@ -59,7 +81,7 @@ Univ.LoadObjects = function(){
 		},
 		function(howmany){ // costs
 			var prices = {
-				qfoam: Math.ceil(1000 * Math.pow(1.02,this.number)) // doesn't work for buying more than 1
+				qfoam: Math.ceil(1000 * Math.pow(0.999,this.number)) // doesn't work for buying more than 1
 			}
 			return prices;
 		},
@@ -211,20 +233,20 @@ Univ.LoadObjects = function(){
 			}
 			return consumption;
 		});
-	new Univ.Object('atom1','atom','Hydrogen Condenser','Hydrogen Condensers',0,
+	new Univ.Object('lightatom1','lightatom','Hydrogen Condenser','Hydrogen Condensers',0,
 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 		function(){ // isVisible
 			return 1;
 		},
 		function(howmany){ // costs
 			var prices = {
-				subatomic: Math.ceil(90 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+				subatomic: 90
 			}
 			return prices;
 		},
 		function(number){ // production
 			var production = {
-				atom: 1 * number
+				lightatom: 1 * number
 			}
 			return production;
 		},
@@ -234,20 +256,20 @@ Univ.LoadObjects = function(){
 			}
 			return consumption;
 		});
-	new Univ.Object('atom2','atom','Isotope Organizer','Isotope Organizers',0,
+	new Univ.Object('lightatom2','lightatom','Isotope Organizer','Isotope Organizers',0,
 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 		function(){ // isVisible
 			return 1;
 		},
 		function(howmany){ // costs
 			var prices = {
-				subatomic: Math.ceil(1800 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+				subatomic: 1800
 			}
 			return prices;
 		},
 		function(number){ // production
 			var production = {
-				atom: 17.5 * number
+				lightatom: 17.5 * number
 			}
 			return production;
 		},
@@ -257,50 +279,354 @@ Univ.LoadObjects = function(){
 			}
 			return consumption;
 		});
-	new Univ.Object('atom3','atom','Quantum Degenerator','Quantum Degenerators',0,
+	new Univ.Object('lightatom3','lightatom','Quantum Degenerator','Quantum Degenerators',0,
 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 		function(){ // isVisible
 			return 1;
 		},
 		function(howmany){ // costs
 			var prices = {
-				subatomic: Math.ceil(36000 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+				subatomic: 36000
 			}
 			return prices;
 		},
 		function(number){ // production
 			var production = {
-				atom: 350 * number
+				lightatom: 350 * number
 			}
 			return production;
 		},
+		
+		
+		//Univ.Items['gascloud'].number;
+		//Univ.Items['lightatom'].number = 149900000;
+
+		
 		function(number){ // consumption
 			var consumption = {
 				subatomic: 500 * number
 			}
 			return consumption;
 		});
-	new Univ.Object('atom4','atom','Heavy Element Engine (Star)','Heavy Element Engines (Stars)',0,
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		new Univ.Object('gascloud1','gascloud','Interstellar Medium','Interstellar Medium',0,
+		'Some say it can predict the future.',
 		function(){ // isVisible
 			return 1;
 		},
 		function(howmany){ // costs
 			var prices = {
-				atom: Math.ceil(720000 * Math.pow(0.998,this.number)) // doesn't work for buying more than 1
+				lightatom: 10000000 * howmany
 			}
 			return prices;
 		},
 		function(number){ // production
 			var production = {
-				atom: 7000 * number
+				gascloud: 0.01 * number
 			}
 			return production;
 		},
 		function(number){ // consumption
 			var consumption = {
-//				subatomic: 8000 * number,
-				atom: 3000 * number // to simulate turning lower elements into higher ones? -- probably unnecessary
+				lightatom: 150000000 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('gascloud2','gascloud','Interstellar Large','Interstellar Large',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				lightatom: 40000000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				gascloud: 0.05 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				lightatom: 150000000 * 4.5 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('gascloud3','gascloud','Interstellar XL','Interstellar XL',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				lightatom: 160000000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				gascloud: 0.25 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				lightatom: 150000000 * 18 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('nebula1','nebula','Nebulizer','Nebulizers',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				gascloud: 200 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				nebula: 0.001 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				gascloud: 0.020 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('nebula2','nebula','Rosette Polarizer','Rosette Polarizers',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				gascloud: 2000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				nebula: 0.012 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				gascloud: 0.240 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('nebula3','nebula','Orionizer','Orionizers',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				gascloud: 20000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				nebula: 0.0150 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				gascloud: 3 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('star1','star','Core Nucleator','Core Nucleators',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				nebula: 123 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				star: 0.1 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				nebula: 0.0333 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('star2','star','Bok Globule','Bok Globules',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				nebula: 1234 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				star: 0.05 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				nebula: 0.01 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('star3','star','Stellar Nursery','Stellar Nurseries',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				nebula: 12345 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				star: 0.025 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				nebula: 0.00333 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('heavyatom1','heavyatom','Main Sequence Ignition','Main Sequence Ignition',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				star: 155 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				heavyatom: 9000000 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				lightatom: 150000000 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('heavyatom2','heavyatom','Convection Zoning Law','Convection Zoning Laws',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				star: 1777 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				heavyatom: 900000000 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				lightatom: 15000000000 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('heavyatom3','heavyatom','Cosmic Spallation','Cosmic Spallation',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				qfoam: 10000000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				heavyatom: 10000000 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				lightatom: 500000000 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('heavyatom4','heavyatom','Supernova Spark','Supernova Sparks',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				star: 313 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				heavyatom: 90000000000 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				star: 1000 * number
+			}
+			return consumption;
+		});
+		new Univ.Object('galaxy1','galaxy','Accretion Disco','Accretion Discos',0,
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		function(){ // isVisible
+			return 1;
+		},
+		function(howmany){ // costs
+			var prices = {
+				star: 5000 * howmany
+			}
+			return prices;
+		},
+		function(number){ // production
+			var production = {
+				galaxy: 0.0001 * number
+			}
+			return production;
+		},
+		function(number){ // consumption
+			var consumption = {
+				star: 120 * number
 			}
 			return consumption;
 		});

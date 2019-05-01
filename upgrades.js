3869b182d0500bc05e9509a033d14af0a29d2c0f
klattmose <mattklose@gmail.com>
2019-05-01 16:45:13 -0400

    Make Cost, Production, and Consumption equations more flexible in terms of upgrades and also in general
    CostEquation, ProductionEquation, and ConsumptionEquation can now all be functions that return the equation object as well as just the equation object. Upgrades can be of type 'multiply' or 'add', and magnitude can be either a number or a function that returns a number.

--- upgrades.js
+++ upgrades.js
@@ -4,7 +4,8 @@ Univ.LoadUpgrades = function(){
 /**
 TYPES:
 	'speed'			: Changes the production interval of a generator
-	'multiply'		: Multiplies Production and Consumption by factor
+	'multiply'		: Multiplies Cost, Production, and Consumption by magnitude. Magnitude can be a function.
+	'add'			: Adds magnitude to Cost, Production, and Consumption before multiplying. Magnitude can be a function.
 	'efficiency'	: Multiplies Consumption by factor
 	'costMult'		: Multiplies Costs by factor
 
